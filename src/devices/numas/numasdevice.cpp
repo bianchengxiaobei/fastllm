a52d3c96cfebb5d06b464ab02fc94fc63d76e49b
Caomao <443850741@qq.com>
2026-08-23 12:29:11 +0800

    debug

--- src/devices/numas/numasdevice.cpp
+++ src/devices/numas/numasdevice.cpp
@@ -7357,16 +7357,15 @@ namespace fastllm {
                     int workersPerNode = numaConfig->numaToCpuDict[0].size();
                     // Bytes scanned per token: gate weight (inputDim x interDim*2 x 2B x experts)
                     // + down weight (interDim x outputDim x 2B x experts), read once per token.
-                    double gateBytes = (double)inputDim * (interDim * 2.0) * 2.0 * totalExperts;
-                    double downBytes = (double)interDim * outputDim * 2.0 * totalExperts;
+                    double gateBytes = (double)inputDim * (interDim * 2.0) * 2.0 * profileExpertCalls;
+                    double downBytes = (double)interDim * outputDim * 2.0 * profileExpertCalls;
                     double gateSec = profileGateMs / 1000.0;
                     double downSec = profileDownMs / 1000.0;
                     double gateGBs = gateSec > 0 ? gateBytes / gateSec / 1e9 : 0;
                     double downGBs = downSec > 0 ? downBytes / downSec / 1e9 : 0;
-                    printf("[fastllm-profile-numas-moe] small_batch bs=%d topk=%d experts=%d inputDim=%d interDim=%d outputDim=%d gateType=%d downType=%d gateRowsPerTask=%d downRowsPerTask=%d numaCnt=%d threads=%d workersPerNode=%d register=%.3f resize=%.3f input=%.3f gate_prep=%.3f gate_swiglu=%.3f(%.1fGB/s %dMB) swiglu_convert=%.3f down_prep=%.3f down=%.3f(%.1fGB/s %dMB) reduce=%.3f output=%.3f total=%.3f\n",
+                    printf("[fastllm-profile-numas-moe] small_batch bs=%d topk=%d experts=%d inputDim=%d interDim=%d outputDim=%d gateType=%d downType=%d numaCnt=%d threads=%d workersPerNode=%d register=%.3f resize=%.3f input=%.3f gate_prep=%.3f gate_swiglu=%.3f(%.1fGB/s %dMB) swiglu_convert=%.3f down_prep=%.3f down=%.3f(%.1fGB/s %dMB) reduce=%.3f output=%.3f total=%.3f\n",
                            bs, topk, profileExpertCalls, inputDim, interDim, outputDim,
                            (int)weights[2]->GetDataType(), (int)weights[3]->GetDataType(),
-                           gateRowsPerTask, downRowsPerTask,
                            numaConfig->numaCnt, numaConfig->threads, workersPerNode,
                            profileRegisterMs, profileResizeMs, profileInputMs,
                            profileGatePrepMs, profileGateMs, gateGBs, (int)(gateBytes / 1e6),
